ec7ae163f371afb7847ba5578c69be7f4f6d5db1
zhang.lijuan78 <zhang.lijuan78@gmail.com>
2020-06-30 16:38:49 -0700

    fix bug

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/lizhieffe/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/lizhieffe/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_GRV, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                          KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                       KC_LBRC,KC_LALT,                                                       KC_LEFT, KC_RIGHT,
-                                      MT(MOD_HYPR,KC_SPC),MT(MOD_LCTL,KC_BSPC),                        MT(MOD_LSFT,KC_ENT), LT(_RAISE, KC_ESC),
+                                      MT(MOD_LGUI,KC_SPC),MT(MOD_LCTL,KC_BSPC),                        MT(MOD_LSFT,KC_ENT), LT(_RAISE, KC_ESC),
                                       KC_BSPC,KC_SPC,                         KC_ENT,LT(_LOWER, KC_TAB),
                                       KC_DEL, KC_GRV,                        KC_LBRC,KC_RBRC
   ),

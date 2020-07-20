48d1fed4b0550b9a5445e5a853e794139f4135eb
zhang.lijuan78 <zhang.lijuan78@gmail.com>
2020-07-20 09:12:11 -0700

    update

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/lizhieffe/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/lizhieffe/keymap.c
@@ -12,6 +12,7 @@
 
 enum custom_keycodes {
     VIM_CLANGFORMAT = SAFE_RANGE,
+    VIM_BRACLET,
     GMAIL_DELETE,
     CHROME_LEFT_TAB,
     CHROME_RIGHT_TAB,
@@ -22,9 +23,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case VIM_CLANGFORMAT:
         if (record->event.pressed) {
             // when keycode VIM_CLANGFORMAT is pressed
-            SEND_STRING(SS_LSFT(";")"ClangFormat\n");
+            SEND_STRING(SS_TAP(X_ESC)SS_LSFT(";")"ClangFormat\n");
         } else {
             // when keycode VIM_CLANGFORMAT is released
+        }
+        break;
+    case VIM_BRACLET:
+        if (record->event.pressed) {
+            // when keycode VIM_BRACLET is pressed
+            SEND_STRING(SS_TAP(X_ESC)"a{\n\n}"SS_TAP(X_ESC)"ka");
+        } else {
+            // when keycode VIM_BRACLET is released
         }
         break;
     case GMAIL_DELETE:
@@ -81,20 +90,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL,
      CHROME_LEFT_TAB, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     CHROME_RIGHT_TAB, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                          KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                      GMAIL_DELETE,KC_LALT,                                                       KC_LEFT, KC_RIGHT,
+     GMAIL_DELETE, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                          KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+                      CHROME_LEFT_TAB,CHROME_RIGHT_TAB,                                                       KC_LEFT, KC_RIGHT,
                                       MT(MOD_LGUI,KC_ESC),KC_LCTL,                        KC_SPC,LT(_RAISE, KC_ENT),
                                       RAISE,KC_LSFT,                         KC_BSPC,LT(_LOWER, KC_TAB),
-                                      LOWER, CHROME_RIGHT_TAB,                        KC_DEL,KC_RBRC
+                                      LOWER, KC_LALT,                        KC_DEL,KC_RBRC
   ),
 
   [_RAISE] = LAYOUT_5x6(
        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
        KC_MS_ACCEL0,_______,_______,KC_MS_UP,_______,KC_LBRC,                        KC_RBRC,KC_PGDOWN,KC_PGUP,KC_INS ,KC_SLCK,KC_MUTE,
-       KC_MS_ACCEL1,_______,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,_______,                        KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,KC_VOLU,
-       KC_MS_ACCEL2,_______,KC_MS_WH_LEFT,_______,KC_MS_WH_RIGHT,_______,                        _______,VIM_CLANGFORMAT,_______,_______,_______,KC_VOLD,
+       KC_MS_ACCEL1,_______,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_MS_BTN1,                        KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,KC_VOLU,
+       KC_MS_ACCEL2,_______,KC_MS_WH_LEFT,_______,KC_MS_WH_RIGHT,KC_MS_BTN2,                        VIM_BRACLET,VIM_CLANGFORMAT,_______,_______,_______,KC_VOLD,
                                                KC_MS_BTN4,KC_MS_BTN5,            KC_EQL ,_______,
-                                               KC_MS_BTN1,KC_MS_BTN2,            _______,_______,
+                                               _______,_______,            _______,_______,
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______
   ),
